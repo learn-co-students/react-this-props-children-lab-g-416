a08994463b7efc34511880c1fb13ff6667f77a58
evanscloud <dijkstrascloud@gmail.com>
2017-01-09 13:48:34 -0500

    Create invitation and decorations component to render children props

---
+++ components/ThemedDecorations.js
@@ -0,0 +1,18 @@
+const React = require('react');
+
+class ThemedDecorations extends React.Component {
+  render () {
+
+    const childrenWithProps = React.Children.map ( this.props.children, child => {
+      return React.cloneElement ( child, {
+        className: this.props.theme
+      });
+    });
+
+    return (
+      <p>{ childrenWithProps }</p>
+    );
+  }
+}
+
+module.exports = ThemedDecorations;
