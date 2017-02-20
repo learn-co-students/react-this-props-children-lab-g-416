3848b26c652ca92412570fbb93770dc3e046475d
Dom McKellar <dom.mckellar@gmail.com>
2017-02-20 12:18:06 -0800

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
-
 const Invitation = require('./components/Invitation');
 const ThemedParty = require('./components/ThemedDecorations');
 
 ReactDOM.render(
   <div>
     <Invitation>Hey, you should totally come to this awesome party I'm throwing!</Invitation>
+
     <ThemedParty theme="heaven">
       <p>A decoration</p>
       <p>Some other decoration</p>
